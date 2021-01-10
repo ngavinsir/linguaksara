2859629e80fa623e39bdf510ce3d5d090ac51d05
ngavinsir <ngavinsir@gmail.com>
2021-01-10 21:46:42 +0700

    move old linguaksara to clickbait route

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,16 +1,9 @@
-<script>
-	import Axios from "../components/Axios.svelte";
-	import Notification from "../components/Notification.svelte";
-	import Modal from '../components/Modal.svelte';
-	import utc from 'dayjs/plugin/utc';
-	import dayjs from 'dayjs';
-	dayjs.extend(utc);
-</script>
-
 <svelte:head>
-	<link rel="dns-prefetch" href="//fonts.googleapis.com">
-  	<link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
-  	<link href="https://fonts.googleapis.com/css2?family=Merriweather:wght@300;400;700&family=Source+Sans+Pro:wght@400;700&display=swap" rel="stylesheet">
+	<link rel="dns-prefetch" href="//fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin />
+	<link
+		href="https://fonts.googleapis.com/css2?family=Merriweather:wght@300;400;700&family=Source+Sans+Pro:wght@400;700&display=swap"
+		rel="stylesheet" />
 	<style>
 		html {
 			height: 100%;
@@ -22,6 +15,4 @@
 	</style>
 </svelte:head>
 
-<Modal><Notification><Axios>
-	<slot></slot>
-</Axios></Notification></Modal>
+<slot />
---
+++ src/routes/clickbait/_layout.svelte
@@ -0,0 +1,16 @@
+<script>
+	import Axios from "../../components/Axios.svelte";
+	import Notification from "../../components/Notification.svelte";
+	import Modal from "../../components/Modal.svelte";
+	import utc from "dayjs/plugin/utc";
+	import dayjs from "dayjs";
+	dayjs.extend(utc);
+</script>
+
+<Modal>
+	<Notification>
+		<Axios>
+			<slot />
+		</Axios>
+	</Notification>
+</Modal>
